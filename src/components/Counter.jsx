98fec3daeb228b4ed2cd23c89a4cd360fcea7c2f
Gumaro Monroy <maromvz@gmail.com>
2023-12-07 13:53:44 -0600

    Add example components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { add, substract } from "../slices/exampleSlice";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const { counter } = useSelector((state) => state.example)
+
+  function addToCounter() {
+    dispatch(add())
+  }
+
+  function substractToCounter() {
+    dispatch(substract())
+  }
+  return (
+    <div id="counter">
+      <h2>{counter}</h2>
+      <button type='button' onClick={addToCounter}>Increase</button>
+      <button type='button' onClick={substractToCounter}>Decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Counter from "./Counter";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to the home page</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default Home;
