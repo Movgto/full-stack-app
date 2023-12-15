e5d59652ae7369e0e71802228978999d3606dde4
Gumaro Monroy <maromvz@gmail.com>
2023-12-15 11:00:35 -0600

    Add home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
-import Counter from './Counter';
+import { NavLink, redirect } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const Home = () => (
-  <>
-    <h1>Welcome to the home page</h1>
-    <Counter />
-  </>
-);
+const Home = () => {
+
+  useEffect(()=>{
+    user = JSON.parse(localStorage.getItem('user'));
+    if (user.username) { // If user is already logged in redirects to the motors page
+      return redirect('/motors');
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to the home page</h1>
+      <NavLink to='/login'>Log In</NavLink>
+      <NavLink to='/signin'>Sign In</NavLink>
+    </>
+  );
+}
 
 export default Home;
