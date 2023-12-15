c0131eb71e50a4bb0cfa2f3e604de22485aab8e5
Gumaro Monroy <maromvz@gmail.com>
2023-12-15 15:05:48 -0600

    Update routes
    Added a nested route to share layout for children routes, and an index route to render an element for the parent route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Reserve from './routes/Reserve';
+import Layout from './components/layout/Layout';
+import Home from './routes/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/reserve" element={<Reserve />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="reserve" element={<Reserve />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
