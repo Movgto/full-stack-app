11c88c664ff7eb95db64da032d6d0447419f7ce9
Cristian Villa <c.villavirgen@gmail.com>
2023-12-20 13:14:29 -0600

    Fix eslint issues

--- src/components/_mock_/ReservationForm.jsx
+++ src/components/_mock_/ReservationForm.jsx
@@ -3,34 +3,40 @@ import React from 'react';
 const ReservationForm = () => (
   <div className="reservation-form">
     {/* Mock content for the reservation form */}
-    <label htmlFor="date" className="block text-gray-600">Date:</label>
-    <input
-      type="date"
-      id="date"
-      name="date"
-      className="w-full px-4 py-2 rounded-md text-white bg-inputBg focus:bg-inputBg border-gray-300 focus:outline-none focus:border-blue-500"
-    />
+    <label htmlFor="date" className="block text-gray-600">
+      Date:
+      <input
+        type="date"
+        id="date"
+        name="date"
+        className="w-full px-4 py-2 rounded-md text-white bg-inputBg focus:bg-inputBg border-gray-300 focus:outline-none focus:border-blue-500"
+      />
+    </label>
 
-    <label htmlFor="motorcycle" className="block text-gray-600">Motorcycle:</label>
-    <input
-      type="text"
-      id="item_id"
-      name="item_id"
-      className="w-full px-4 py-2 rounded-md text-white focus:outline-none focus:border-blue-500  bg-inputBg focus:bg-inputBg"
-      value="Mock Motorcycle"
-    />
+    <label htmlFor="item_id" className="block text-gray-600">
+      Motorcycle:
+      <input
+        type="text"
+        id="item_id"
+        name="item_id"
+        className="w-full px-4 py-2 rounded-md text-white focus:outline-none focus:border-blue-500 bg-inputBg focus:bg-inputBg"
+        value="Mock Motorcycle"
+      />
+    </label>
 
-    <label htmlFor="city" className="block text-gray-600">Location:</label>
-    <select
-      id="city"
-      name="city"
-      className="w-full border-gray-300 focus:outline-none bg-inputBg focus:bg-inputBg focus:border-customBg"
-    >
-      <option value="New York">New York</option>
-      <option value="Los Angeles">Los Angeles</option>
-      <option value="Chicago">Chicago</option>
-      {/* Add more mock options as needed */}
-    </select>
+    <label htmlFor="city" className="block text-gray-600">
+      Location:
+      <select
+        id="city"
+        name="city"
+        className="w-full border-gray-300 focus:outline-none bg-inputBg focus:bg-inputBg focus:border-customBg"
+      >
+        <option value="New York">New York</option>
+        <option value="Los Angeles">Los Angeles</option>
+        <option value="Chicago">Chicago</option>
+        {/* Add more mock options as needed */}
+      </select>
+    </label>
 
     <button
       type="submit"
@@ -41,4 +47,5 @@ const ReservationForm = () => (
     {/* You can add more mock elements here */}
   </div>
 );
+
 export default ReservationForm;
